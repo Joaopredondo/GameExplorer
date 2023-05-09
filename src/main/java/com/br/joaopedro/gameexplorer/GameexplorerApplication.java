8425d04de1bbd8b9adfa2baef94823c123b590ee
Joaopredondo <jaop123456789@hotmail.com>
2023-05-09 19:29:16 -0300

    Inicio do projeto

---
+++ src/main/java/com/br/joaopedro/gameexplorer/GameexplorerApplication.java
@@ -0,0 +1,13 @@
+package com.br.joaopedro.gameexplorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameexplorerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameexplorerApplication.class, args);
+	}
+
+}
